c664276ed21525d3f0e23ac75149372e0dcfcd8b
Kirill <kpaskevic39@gmail.com>
2025-03-09 18:41:13 +0200

    feat/add branch develop and create function Fahrenheit to Celsius

---
+++ src/com/utils/Convertor.java
@@ -0,0 +1,8 @@
+package com.utils;
+
+public class Convertor {
+
+    public static double getConverterFtoC(double Celsius) {
+        return (Celsius * 9 / 5) + 32; // ( °C × 9/5) + 32 =  °F
+    }
+}
